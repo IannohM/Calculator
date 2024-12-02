74eb8ecea8837e9ef70adfe727643ee1734e15e0
IannohM <mutukuianmusyoka@duck.com>
2024-12-02 11:49:16 +0300

    initialised the project

---
+++ js/calculator.js
@@ -0,0 +1,21 @@
+const inputValue = document.querySelector('.input-sector');
+function displayUserInput(input){
+    inputValue.value =inputValue.value + input;
+
+
+}
+
+
+function clearSreen(){
+    inputValue.value = '';
+}
+
+function calculate(){
+    try{
+        inputValue.value = eval(inputValue.value)
+    }
+    catch(error){
+        inputValue.value = "Syntax Error"
+    }
+
+}
